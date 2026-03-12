257ad1ed3fb47a329fb1896d0782239241ab1b43
v-63fcav <v-63fcav@hotmail.com>
2026-03-12 18:31:58 -0300

    cleanup for tomorrow

--- apps/amp.tf
+++
@@ -1,9 +0,0 @@
-# Amazon Managed Prometheus Workspace
-resource "aws_prometheus_workspace" "main" {
-  alias = "eks-study-prometheus"
-  tags = {
-    Environment = "eks-study"
-    ManagedBy   = "terraform"
-    Project     = "eks-study"
-  }
-}
--- apps/variables.tf
+++ apps/variables.tf
@@ -12,9 +12,3 @@ variable "cluster_name" {
 variable "kube_host" {}
 variable "kube_ca" {}
 variable "alb_irsa_role" {}
-
-# Read from infra remote state
-locals {
-  oidc_provider_arn = data.terraform_remote_state.infra.outputs.oidc_provider_arn
-  adot_irsa_role   = data.terraform_remote_state.infra.outputs.adot_irsa_role
-}
