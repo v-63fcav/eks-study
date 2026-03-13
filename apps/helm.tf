2bb40ab18c5d2fdee7352d89a5508b206db301bf
v-63fcav <v-63fcav@hotmail.com>
2026-03-13 13:51:07 -0300

    feat: add kube-prometheus-stack Helm release and configuration values

--- apps/helm.tf
+++ apps/helm.tf
@@ -6,8 +6,24 @@ resource "helm_release" "alb_controller" {
 
   values = [
     templatefile("${path.module}/values/values-alb-controller.yaml", {
-      cluster_name = var.cluster_name
+      cluster_name  = var.cluster_name
       alb_irsa_role = var.alb_irsa_role
     })
   ]
+}
+
+resource "helm_release" "kube_prometheus_stack" {
+  name             = "kube-prometheus-stack"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  version          = "69.3.1"
+  namespace        = "monitoring"
+  create_namespace = true
+
+  values = [file("${path.module}/values/values-kube-prometheus-stack.yaml")]
+
+  # CRDs can be large; increase timeout for first install
+  timeout = 600
+
+  depends_on = [kubernetes_storage_class_v1.gp3]
 }
